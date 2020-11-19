0311b6305dd116a10d61e8a87a3f7e0ea776dc9b
mud-skip <74700432+mud-skip@users.noreply.github.com>
2020-11-19 18:23:16 +1100

    Update Rcode.R

--- Rcode.R
+++ Rcode.R
@@ -1,15 +1,23 @@
 library(tidyverse)
 library(ggplotlyExtra)
 
-a <- read_delim("../micro/chrnames.txt", "\t")
+setwd("../micro/")
+
+a <- read_delim("chrnames.txt", "\t")
 a <- a %>% group_by(assemblyid) %>% mutate(krank=row_number(), offset = lag(length), offset = replace_na(offset, 0), offset = cumsum(offset)) %>% ungroup()
 
+#> files
+#[1] "GCF_000002315.6_GRCg6a.vs.GCA_000241765.4_Chrysemys_picta_BioNano-3.0.4.chainpairs.tab" "GCF_000002315.6_GRCg6a.vs.GCA_002798355.1_Ogye1.0.chainpairs.tab"          
+#[3] "GCF_000002315.6_GRCg6a.vs.GCA_003400415.2_UTA_CroVir_3.0.chainpairs.tab"                "GCF_000002315.6_GRCg6a.vs.GCA_009733165.1_Nana_v5.chainpairs.tab"          
+#[5] "GCF_000002315.6_GRCg6a.vs.GCA_009764565.2_rDerCor1.pri.v2.chainpairs.tab"               "GCF_000002315.6_GRCg6a.vs.GCA_009769625.1_bCygOlo1.pri.chainpairs.tab"     
+#[7] "GCF_000002315.6_GRCg6a.vs.GCA_013407035.1_ASM1340703v1.chainpairs.tab"                  "GCF_000002315.6_GRCg6a.vs.GCA_014706415.1_LU_Pmuni_1.1.chainpairs.tab"     
+#[9] "GCF_000002315.6_GRCg6a.vs.GCA_015237465.1_rCheMyd1.pri.chainpairs.tab"                  "GCF_000002315.6_GRCg6a.vs.GCF_000002315.6_GRCg6a.chainpairs.tab"           
+#[11] "GCF_000002315.6_GRCg6a.vs.GCF_000090745.1_AnoCar2.0.chainpairs.tab"                     "GCF_000002315.6_GRCg6a.vs.GCF_004115215.1_mOrnAna1.p.v1.chainpairs.tab"   
+#[13] "GCF_000002315.6_GRCg6a.vs.GCF_004329235.1_PodMur_1.0.chainpairs.tab"                    "GCF_000002315.6_GRCg6a.vs.GCF_007399415.2_rGopEvg1_v1.p.chainpairs.tab"   
+#[15] "GCF_000002315.6_GRCg6a.vs.GCF_009769535.1_rThaEle1.pri.chainpairs.tab"                  "GCF_000002315.6_GRCg6a.vs.GCF_009819535.1_rLacAgi1.pri.chainpairs.tab"        
+#[17] "GCF_000002315.6_GRCg6a.vs.GCF_011800845.1_UG_Zviv_1.chainpairs.tab"                     "GCF_000002315.6_GRCg6a.vs.GCF_013100865.1_CAS_Tse_1.0.chainpairs.tab"    
 
 files <- list.files(".", "GCF_000002315.6_GRCg6a.vs.*")
-
-#b %>% mutate(tmid=toffset + ((tstart+tend)/2), qmid=qoffset + ((qstart+qend)/2)) %>% dplyr::filter(qlen>500) %>% ggplot(aes(x=tmid, y=qmid)) + geom_point( size = 0.3) + theme_bw() + scale_x_continuous(breaks = arrange(b, trank) %>% pull(toffset) %>% unique(), labels = arrange(b, trank) %>% pull(tchr) %>% unique()) + scale_y_continuous(breaks = arrange(b, qrank) %>% pull(qoffset) %>% unique(), labels = arrange(b, qrank) %>% pull(qchr) %>% unique())
-#b %>% mutate(tmid=toffset + ((tstart+tend)/2), qmid=qoffset + ((qstart+qend)/2)) %>% dplyr::filter(qlen>500) %>% ggplot() + geom_segment(aes(x=tstart+toffset, y=qstart+qoffset, xend=tend+toffset, yend=qend+qoffset,)) + theme_bw() + scale_x_continuous(breaks = arrange(b, trank) %>% pull(toffset) %>% unique(), labels = arrange(b, trank) %>% pull(tchr) %>% unique()) + scale_y_continuous(breaks = arrange(b, qrank) %>% pull(qoffset) %>% unique(), labels = arrange(b, qrank) %>% pull(qchr) %>% unique())
-#b %>% dplyr::filter(trank>20) %>% mutate(tmid=toffset + ((tstart+tend)/2), qmid=qoffset + ((qstart+qend)/2)) %>% dplyr::filter(qlen>500) %>% ggplot() + geom_segment(aes(x=tstart+toffset, y=qstart+qoffset, xend=tend+toffset, yend=qend+qoffset,)) + theme_bw() + scale_x_continuous(breaks = arrange(b, trank) %>% pull(toffset) %>% unique(), minor_breaks = NULL, labels = arrange(b, trank) %>% pull(tchr) %>% unique()) + scale_y_continuous(breaks = arrange(b, qrank) %>% pull(qoffset) %>% unique(), minor_breaks = NULL, labels = arrange(b, qrank) %>% pull(qchr) %>% unique())
 
 b <- read_delim(files[4], "\t",col_names = F)
 colnames(b) <- c("tacc", "tstart", "tend", "qacc", "qstart", "qend", "strand")
